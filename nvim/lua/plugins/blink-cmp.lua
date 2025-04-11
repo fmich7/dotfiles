58a10176335f2cf90a847c2c9e5fd27c6252752e
fmich7 <michalak.filip.04@gmail.com>
2025-04-11 17:06:46 +0200

    blink-cmp tweaks

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -1,6 +1,52 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "onsails/lspkind.nvim",
+  },
   opts = function(_, opts)
-    opts.completion.ghost_text.enabled = false
+    opts.completion = {
+      ghost_text = {
+        enabled = false,
+      },
+
+      menu = {
+        draw = {
+          columns = { { "kind_icon" }, { "label", gap = 1 } },
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx)
+                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                return kind_icon
+              end,
+              highlight = function(ctx)
+                -- return (
+                -- require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx)
+                -- or "BlinkCmpKind"
+                -- ) .. ctx.kind
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+
+            label = {
+              text = function(ctx)
+                return require("colorful-menu").blink_components_text(ctx)
+              end,
+              highlight = function(ctx)
+                return require("colorful-menu").blink_components_highlight(ctx)
+              end,
+            },
+          },
+        },
+      },
+
+      documentation = {
+        auto_show = true,
+        window = {
+          border = "rounded",
+        },
+      },
+    }
   end,
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,39 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-    semanticTokens = false,
-  },
+  opts = function(_, opts)
+    opts.servers = opts.servers or {}
+
+    opts.inlay_hints = { enabled = false }
+    opts.semanticTokens = { enabled = false }
+
+    -- Lua language server configuration
+    opts.servers.lua_ls = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          hint = {
+            enable = false, -- disables inlay hints
+          },
+          semantic = {
+            enable = false, -- disables semantic tokens
+          },
+        },
+      },
+    }
+
+    -- Setup function for special LSPs
+    opts.setup = opts.setup or {}
+
+    -- Custom clangd setup
+    opts.setup.clangd = function(_, clangd_opts)
+      clangd_opts.capabilities = clangd_opts.capabilities or {}
+      clangd_opts.capabilities.offsetEncoding = { "utf-16" }
+    end
+  end,
 }
